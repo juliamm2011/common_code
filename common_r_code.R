4b7e050972de398d644ad9866241a6962f52ef33
jumorris2017 <33705848+jumorris2017@users.noreply.github.com>
2018-03-08 16:27:08 -0800

    3.8.18

--- common_r_code.R
+++ common_r_code.R
@@ -69,6 +69,9 @@ hrs[hatota>0, hwealthlog := hatota]
 #divide one column by another
 dt <- dt[, newvar := var1/var2] 
 
+#average across a list of columns
+dt[, so_score := rowMeans(subset(temp, select = c(grep("_scr",colnames(dt),value=T))), na.rm = TRUE)]
+
 #ifelse statement in creating a new var
 dt[, newvar := ifelse(othervar=="EXAMPLE", 1, 0)]
 
@@ -404,7 +407,7 @@ DT_odd <- DT_odd[, eval(expr)]
 #remove all "mean" values we have observed data for
 DT_even[var1 > 0, var1_mean := 0]
 #add mean and observed columns together
-DT_even[, var1 :=rowSums(.SD, na.rm = TRUE), .SDcols = c("var1", "var1_mean")]
+DT_even[, var1 := rowSums(.SD, na.rm = TRUE), .SDcols = c("var1", "var1_mean")]
 #subset to only original columns
 DT_even <- DT_even[, eval(expr)]
 #rbind to the odd years
@@ -849,3 +852,49 @@ str2m[, R2MCC := round(R2MTB/R2MRSPNS,3)]
 str2m[, FPFY := paste0(FSCL_YR_NUM,".",FSCL_PER_IN_YR_NUM)]
 str2m[, FPFY := as.numeric(FPFY)]
 str2m[, FPFYlabel := paste0(FSCL_YR_NUM,"-",FSCL_PER_IN_YR_NUM)]
+
+
+
+##line chart split by two variables, with a geom_text line height variable
+
+#make year-month variable for plotting
+can[, fyfp := paste0(FSCL_YR_NUM,".",str_pad(can[,FSCL_PER_IN_YR_NUM],2,pad="0"))]
+
+#make a plotting height label
+can[variable=="SO_SCORE"&area=="Canada", value_y := value+2.5]
+can[variable=="SO_SCORE"&area=="Vancouver", value_y := value-2.5]
+can[variable=="CC_SCORE"&area=="Canada", value_y := value+2.5]
+can[variable=="CC_SCORE"&area=="Vancouver", value_y := value-2.5]
+
+#set labels
+xlabels <- c("Mar 17", "Apr 17", "May 17", "June 17", "July 17", "Aug 17", 
+             "Sep 17", "Oct 17", "Nov 17", "Dec 17", "Jan 18", "Feb 18")
+ylabel <- "TB Score"
+tlabel <- "Vancouver, Canada Customer Experience"
+sublabel <- "Company-Operated Stores, March 2017 - February 2018"
+caption <- "Canada Store N = 1,750\nVancouver Store N = 122"
+#manual legend labels
+lname1 <- "Area"
+llabels1 <- c("Canada","Vancouver")
+lname2 <- "Metric"
+llabels2 <- c("Customer Connection","Store Operations")
+#values
+pdata <- can
+px <- can[,fyfp]
+py <- can[,value]
+groupvar <- can[,variable]
+colourvar <- can[,area]
+#plot itself
+plot2 <- ggplot(data=pdata, aes(x=px, y=py, colour=factor(colourvar), group=interaction(groupvar, colourvar))) + 
+  geom_point(size=1) + geom_line(size=1) +
+  xlab("") + ylab(ylabel) + 
+  scale_x_discrete(labels=xlabels) +
+  scale_colour_discrete(name="", labels=llabels1, guide=guide_legend(order=1)) +
+  guides(colour = guide_legend(override.aes = list(size = 7))) + 
+  scale_y_continuous(limits=c(0,70)) + theme_economist_white(gray_bg = FALSE) +
+  ggtitle(tlabel) + labs(subtitle=sublabel,caption=caption) +
+  annotate(size=5, geom="text", x=1, y=63, label= "Store Operations",hjust = 0) +
+  annotate(size=5, geom="text", x=1, y=38, label= "Customer Connection",hjust = 0) +
+  geom_text(size = 4, aes(label=py,y=value_y), stat="identity")
+print(plot2)
+
